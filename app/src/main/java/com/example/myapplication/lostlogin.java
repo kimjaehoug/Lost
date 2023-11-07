577f90d221fe549390251a140824f66f14309f89
jaehoug <kimjaehoug@gmail.com>
2023-11-08 00:03:00 +0900

    ??

---
+++ app/src/main/java/com/example/myapplication/lostlogin.java
@@ -0,0 +1,71 @@
+package com.example.myapplication;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+public class lostlogin extends AppCompatActivity {
+    private FirebaseAuth mFirebaseAuth; //파이어 베이스 인증
+    private DatabaseReference mDatabaseRef; //실시간 데이터 베이스
+    private EditText mEtEmail, mEtPwd;
+    private Button mBtnRegister;
+    private Button mBtnLogin;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lostlogin);
+
+        mFirebaseAuth = FirebaseAuth .getInstance();
+        mDatabaseRef = FirebaseDatabase.getInstance().getReference();
+
+        mEtEmail = findViewById(R.id.etemail);
+        mEtPwd = findViewById(R.id.etPwd);
+        mBtnRegister = findViewById(R.id.btn_register);
+        mBtnLogin = findViewById(R.id.btn_login);
+
+        mBtnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String strEmail = mEtEmail.getText().toString();
+                String strPwd = mEtPwd.getText().toString();
+
+                mFirebaseAuth.signInWithEmailAndPassword(strEmail,strPwd).addOnCompleteListener(lostlogin.this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+
+                        if(task.isSuccessful()){
+                            //로그인 성공
+                            ;Intent intent = new Intent(lostlogin.this, MapsActivity.class);
+                            startActivity(intent);
+                            finish();
+                        }else{
+                            Toast.makeText(lostlogin.this, "로그인 실패", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+            }
+
+        });
+        mBtnRegister.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                Intent intent = new Intent(lostlogin.this, register.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/example/myapplication/useraccount.java
@@ -0,0 +1,26 @@
+package com.example.myapplication;
+
+public class useraccount {
+
+    public useraccount(){
+    }
+
+
+    public String getIdToken() {return idToken;}
+
+    public void setIdToken(String idToken){
+        this.idToken = idToken;
+    }
+    private String idToken;
+
+    public String getEmailId(){return emailId;}
+    public void setEmailId(String emailId){this.emailId = emailId;}
+    private String emailId;
+
+
+    public String getPassword(){return password;}
+    public void setPassword(String strPwd){this.password = password;}
+    private String password;
+
+}
+
